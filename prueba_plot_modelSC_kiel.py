a7192f0f254369c615138c683ea53d8f7f2a00c6
Diego Vega Gimenez <diegovegagimenez@gmail.com>
2024-07-15 10:45:18 +0200

    Update prueba_plot_modelSC_kiel.py

--- prueba_plot_modelSC_kiel.py
+++ prueba_plot_modelSC_kiel.py
@@ -9,35 +9,32 @@ import xarray as xr
 import pandas as pd
 import mat73
 
+# Paths
 SWOT_path = '/home/dvega/anaconda3/work/SWOT_STORM/'
 model_path = '/home/amores/SWOT/A_data/C_modelo/'
 era5_path = '/home/dvega/anaconda3/work/SWOT_STORM/ERA5/'
 
+# Load data
 model23 = xr.open_dataset(f'{model_path}merged_elevation_2023.nc')
 model24 = xr.open_dataset(f'{model_path}merged_elevation_2024.nc')
-
 model = xr.concat([model23, model24], dim='time')
 model_df = model.to_dataframe().reset_index()
 
+# Filter data for the specific date
 model_df = model_df[(model_df['time']>pd.to_datetime('2023-04-01'))&(model_df['time']<pd.to_datetime('2023-04-02'))]
-
 model_df_kiel = model_df[model_df['time'] == pd.to_datetime('2023-04-01 23:00:00')]
 
-# Plot 1 speciphic date
-lonlatbox = [9, 13, 53.5, 57]
-
+# Extract data
 latSC = model_df_kiel['SCHISM_hgrid_node_y'].values
 lonSC = model_df_kiel['SCHISM_hgrid_node_x'].values
 sshSC = model_df_kiel['elevation'].values
 time = model_df_kiel['time'].values
 
-# Sample data (replace these with your actual data)
+# Define plot bounds and load triangulation data
 latmin, latmax = 53.5, 57
 lonmin, lonmax = 9, 13
-tri = np.random.randint(0, 100, (200, 3))
 lonTG, latTG = np.array([10]), np.array([55])
 peak_date = '2023-04-01 23:00:00'
-
 mat_data = mat73.loadmat(f'{model_path}tri_simu.mat')
 tri = mat_data['tri'].astype(int) - 1
 
@@ -53,8 +50,7 @@ x, y = ax.projection.transform_points(ccrs.PlateCarree(), lonSC, latSC)[:, :2].T
 triang = Triangulation(x, y, tri)
 
 # Plot the triangulated surface
-ax.plot(x, y, 'o', markersize=1)  # Optionally, plot the mesh points
-ax.tripcolor(triang, sshSC, shading='flat', cmap=cmocean.cm.balance)  # Use cmocean's 'balance' colormap
+collection = ax.tripcolor(triang, sshSC, shading='flat', cmap=cmocean.cm.balance)  # Use cmocean's 'balance' colormap
 
 # Add geographic features
 ax.add_feature(cfeature.COASTLINE)
@@ -67,17 +63,14 @@ gl.top_labels = False
 gl.right_labels = False
 
 # Add colorbar
-cbar = plt.colorbar(ax.collections[-1], ax=ax, orientation='vertical', pad=0.02)
+cbar = plt.colorbar(collection, ax=ax, orientation='vertical', pad=0.02)
 cbar.set_label('Sea Level Elevation (m)')
 
 # Plot additional markers
 ax.plot(lonTG, latTG, 'pk', markerfacecolor='k', markersize=10, transform=ccrs.PlateCarree())
 
-# Set color limits
-plt.clim(-0.9, 0.9)
-
-# Set colormap
-plt.set_cmap(cmocean.cm.balance)
+# Set color limits on the collection
+collection.set_clim(-0.9, 0.9)
 
 # Set title
 plt.title(peak_date)
