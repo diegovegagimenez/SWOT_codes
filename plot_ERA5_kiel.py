efc0073fccce7050e0ea2c26fbba88b5dce93e18
Diego Vega Gimenez <diegovegagimenez@gmail.com>
2024-07-16 18:09:27 +0200

    Alteweser added

--- plot_ERA5_kiel.py
+++ plot_ERA5_kiel.py
@@ -5,54 +5,67 @@ import cartopy.feature as cfeature
 import pandas as pd
 import xarray as xr
 
-
+# Load data
 path = '/home/dvega/anaconda3/work/SWOT_STORM/ERA5/'
+ds = xr.open_dataset(f'{path}msl_pressure_global_01-02042023.nc')
+# ds = xr.open_dataset(f'{path}wind_mean_sea_level_pres_kiel.nc')
+ds = xr.open_dataset(f'{path}msl_wind_kiel_v6.nc')
+ds = xr.open_dataset(f'{path}msl_wind_alte_v1.nc')
 
-ds = xr.open_dataset(f'{path}wind_mean_sea_level_pres_kiel.nc')
+# Select specific date
+time_swot = '2023-04-02 00:00:00'
+time_swot = '2023-10-14 17:00:00'
 
-# lon = ds.sel(longitude=slice(9, 13), latitude=slice(53, 59), time='2023-04-02T00:00:00').values
-# lat = ds.sel(longitude=slice(9, 13), latitude=slice(53, 59), time='2023-04-02T00:00:00').values
-msl = ds['msl']
-u10 = ds['u10']
-v10 = ds['v10']
-
-# Select speciphic date
-time_swot = '2023-04-02 00:00:00' 
-u10 = u10.sel(time = time_swot)
-v10 = v10.sel(time = time_swot)
-msl = msl.sel(time = time_swot)
+u10 = ds['u10'].sel(time=time_swot)
+v10 = ds['v10'].sel(time=time_swot)
+msl = ds['msl'].sel(time=time_swot)
 
 # Calculate wind speed and direction
 wind_speed = np.sqrt(u10**2 + v10**2)
-wind_dir = np.arctan2(v10, u10) * (180 / np.pi)
 
-lonlatbox = [9, 12, 53, 59]
-
-fig, ax = plt.subplots(figsize=(10.5, 11), subplot_kw=dict(projection=ccrs.PlateCarree()))
-
-ax.set_extent(lonlatbox)
-
+# Normalize msl to hPa
 msl_hpa = msl / 100.0
-msl_plot = ax.pcolormesh(msl.longitude, msl.latitude, msl_hpa, cmap='viridis', transform=ccrs.PlateCarree())
 
-# Add colorbar for msl plot
-cbar = plt.colorbar(msl_plot, orientation='vertical', pad=0.06, aspect=50)
-cbar.set_label('Mean Sea Level Pressure (hPa)')
+# lolabox = [-10, 20, 10, 70]
 
-# Plot wind vectors
-plt.quiver(u10.longitude, u10.latitude, 
-           u10, v10, 
-           scale=250, transform=ccrs.PlateCarree())
+# Define the plot
+fig = plt.figure(figsize=(10, 5))
+ax = plt.axes(projection=ccrs.PlateCarree())
+
+# Define custom levels for contours
+min_value = np.floor(msl_hpa.min().item())
+max_value = np.ceil(msl_hpa.max().item())
+levels = np.arange(min_value, max_value + 5, 5)
+
+# ax.set_extent(lolabox, crs=ccrs.PlateCarree())
+# Plot mean sea level pressure as contours with thicker grey lines
+msl_contour = ax.contour(msl.longitude, msl.latitude, msl_hpa, levels=levels, colors='#4d4d4d', linewidths=1.5, transform=ccrs.PlateCarree())
+plt.clabel(msl_contour, inline=True, fontsize=8, fmt='%1.0f')
+
+# # Add colorbar for wind speed
+wind_speed_plot = ax.contourf(u10.longitude, u10.latitude, wind_speed, levels=1000, cmap='coolwarm', transform=ccrs.PlateCarree())
+cbar = plt.colorbar(wind_speed_plot, orientation='vertical', pad=0.02, aspect=30)
+cbar.set_label('Wind Speed (m/s)')
+
+stride = 15  # Adjust this value to reduce the number of arrows
+u10_reduced = u10[::stride, ::stride]
+v10_reduced = v10[::stride, ::stride]
+lon_reduced = u10.longitude[::stride]
+lat_reduced = u10.latitude[::stride]
+
+# Plot wind vectors with larger arrows
+plt.quiver(lon_reduced, lat_reduced, u10_reduced, v10_reduced, scale=300, width=0.005, transform=ccrs.PlateCarree())
+
 
 # Add features
 ax.add_feature(cfeature.LAND, zorder=0)
-ax.add_feature(cfeature.COASTLINE, zorder=1, )
-ax.add_feature(cfeature.BORDERS, linestyle=':')
-
-ax.gridlines(draw_labels=True, linestyle='--')
+ax.add_feature(cfeature.COASTLINE, zorder=1)
+gl = ax.gridlines(draw_labels=True, alpha=0.5)
+gl.top_labels = False
+gl.right_labels = False
 
 # Add title and labels
-plt.title('ERA5 Wind Vectors at 10m')
+plt.title(f'{time_swot} ')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 
