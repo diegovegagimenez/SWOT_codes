a3468b5c6d916e76a2dd5a7fb7e7ef98e7054de0
Diego Vega Gimenez <diegovegagimenez@gmail.com>
2024-07-23 08:36:15 +0200

    Paralelization

--- P1_sacar_serie_SWOT_en_TGs.py
+++ P1_sacar_serie_SWOT_en_TGs.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from utide import solve, reconstruct
 import cartopy.crs as ccrs
 import xarray.plot as xplt  # Import xarray.plot module
+import cartopy.feature as cfeature
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 def detide(time_series, ssha_series, latitude):
     """
@@ -85,15 +88,16 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371
     return c * r
 
-dmin = 50  # km de radio
+dmin = 10  # km de radio
 thres = 20*dmin/100
 
 TG_path='/home/amores/SWOT/A_data/A_TGs/'
 # TG_path = ['/home/amores/SWOT/A_data/A_TGs/TG_CMEMS/', '/home/amores/SWOT/A_data/A_TGs/TG_SOEST/']
 SWOT_path = '/home/dvega/anaconda3/work/SWOT_STORM/SWOT_data/'
+model_path = '/home/amores/SWOT/A_data/C_modelo/'
 
-# SWOTfiles = [f for f in os.listdir(SWOT_path) if f.endswith('.nc')]
-# TGfiles = [f for f in os.listdir(TG_path) if f.endswith('.nc')]
+SWOTfiles = [f for f in os.listdir(SWOT_path) if f.endswith('.nc')]
+TGfiles = [f for f in os.listdir(TG_path) if f.endswith('.nc')]
 
 kiel_tg = xr.open_dataset(f'{TG_path}NO_TS_TG_KielTG.nc')
 Alte_tg = xr.open_dataset(f'{TG_path}NO_TS_TG_AlteWeserTG.nc')
@@ -105,7 +109,6 @@ kiel_tg = kiel_tg.to_dataframe().reset_index(level='DEPTH', drop=True)
 Alte_tg = Alte_tg.to_dataframe().reset_index(level='DEPTH', drop=True)
 
 TG_data = [kiel_tg, Alte_tg]
-
 
 lonNames=['lon','longitude','LONGITUDE']
 latNames=['lat','latitude','LATITUDE']
@@ -126,133 +129,238 @@ all_altimetry_timeseries = []
 
 fileSWOT = [os.path.join(SWOT_path, f) for f in os.listdir(SWOT_path) if f.endswith('.nc')]
 
-for filename in tqdm(fileSWOT, desc='SWOT files'):
-    file_path = os.path.join(SWOT_path, filename)
-    ds = xr.open_dataset(file_path)
-
+def process_file(filename):
+    ds = xr.open_dataset(filename)
     ds = ds.drop_dims('num_nadir')
     ds = ds[['time', 'ssha', 'latitude', 'longitude', 'dac']]
-    
-    ssh_dac = ds.ssha+ds.dac
-
-    # Extract data from variables
+    ssh_dac = ds.ssha + ds.dac
     lon = ds['longitude'].values.flatten()
     lat = ds['latitude'].values.flatten()
     ssh = ds['ssha'].values.flatten()
     ssh_dac = ssh_dac.values.flatten()
-    # ssh = ds['ssha'].values.flatten()
-
-    time_values = ds['time'].values  # Adding a new
-    time = np.tile(time_values[:, np.newaxis], (1, 69)).flatten()  # Not efficient
-
-    # Find indices of non-NaN values
+    time_values = ds['time'].values
+    time = np.tile(time_values[:, np.newaxis], (1, 69)).flatten()
     valid_indices = np.where(~np.isnan(ssh))
     lonSWOT = lon[valid_indices]
     latSWOT = lat[valid_indices]
     timeSWOT = time[valid_indices]
     ssh = ssh[valid_indices]
     ssh_dac = ssh_dac[valid_indices]
+    results = []
 
-    # Loop through each tide gauge location
     for idx, (lon_tg, lat_tg) in enumerate(zip(lonTGs, latTGs)):
-
-        # d = np.sqrt((lonSWOT - lon_tg) ** 2 + (latSWOT - lat_tg) ** 2)
-
-        # if np.min(d) > thres:
-        #     continue
-
-        # # Mask distances greater than threshold
-        # mask = d <= thres
-        # alon = np.where(mask, lonSWOT, np.nan)
-        # alat = np.where(mask, latSWOT, np.nan)
-
-        # Calculate distance for each data point
         distances = haversine(lonSWOT, latSWOT, lon_tg, lat_tg)
-
         in_radius = distances <= dmin
 
-        # Average nearby SSH values (if any)
         if np.any(in_radius):
-            print(f'there is data')
-
             ssh_tmp = np.nanmean(ssh[in_radius])
-            ssh_serie = ssh_tmp * 100  # Convert to centimeters (cm)
-            time_serie = timeSWOT[in_radius][~np.isnan(timeSWOT[in_radius])][0]  # Picking the first value of time within the radius
-            ssh_dac_serie = np.nanmean(ssh_dac[in_radius])*100  # To cm
-            
-            # Store the latitudes and longitudes of SWOT within the radius
+            ssh_serie = ssh_tmp * 100
+            time_serie = timeSWOT[in_radius][~np.isnan(timeSWOT[in_radius])][0]
+            ssh_dac_serie = np.nanmean(ssh_dac[in_radius]) * 100
             lat_within_radius = latSWOT[in_radius]
             lon_within_radius = lonSWOT[in_radius]
-
-            # Store closest  distance and number of points used for the average
             min_distance_point = distances[in_radius].min()
-            n_idx = sum(in_radius)  # How many values are used for compute the mean value
-
+            n_idx = sum(in_radius)
         else:
-            # print(f'there is no data')
-            ssh_serie = np.nan  # No data within radius (remains NaN)
+            ssh_serie = np.nan
             time_serie = timeSWOT[in_radius][~np.isnan(timeSWOT[in_radius])]
-            n_idx = np.nan  # Number of points for the average within the radius
+            n_idx = np.nan
             min_distance_point = np.nan
             ssh_dac_serie = np.nan
-
-            # If there's no SWOT data within the radius, set latitudes and longitudes to None
             lat_within_radius = None
             lon_within_radius = None
-            # print(f"No SWOT data within {dmedia} km radius of tide gauge {sorted_names[idx]}")
 
-        # Create a dictionary to store tide gauge and SWOT data
         selected_data = {
-            # "station": filename,  # Access station name
-            "longitude": lon_tg[0],  # Longitude of tide gauge
-            "latitude": lat_tg[0],  # Latitude of tide gauge
-            "ssha": ssh_serie, # Retrieved SSH value
+            "longitude": lon_tg[0],
+            "latitude": lat_tg[0],
+            "ssha": ssh_serie,
             "ssha_dac": ssh_dac_serie,
-            # "ssha_raw": ssh[in_radius],  # Raw SSH values within the radius
             "time": time_serie,
-            "n_val": n_idx,  # Number of points for the average within the radius
-            "lat_within_radius": lat_within_radius,  # Latitudes of SWOT within the radius
-            "lon_within_radius": lon_within_radius,   # Longitudes of SWOT within the radius
-            "min_distance": min_distance_point,  # Closest distance within the radius
-            }
-        all_altimetry_timeseries.append(selected_data)
+            "n_val": n_idx,
+            "lat_within_radius": lat_within_radius,
+            "lon_within_radius": lon_within_radius,
+            "min_distance": min_distance_point,
+        }
+        results.append(selected_data)
+    return results
+
+with ThreadPoolExecutor(max_workers=40) as executor:
+    futures = {executor.submit(process_file, filename): filename for filename in fileSWOT}
+
+    for future in tqdm(as_completed(futures), total=len(futures), desc='Processing SWOT files'):
+        try:
+            results = future.result()
+            all_altimetry_timeseries.extend(results)
+        except Exception as e:
+            print(f"Error processing file {futures[future]}: {e}")
 
 df = pd.DataFrame(all_altimetry_timeseries)
-df.dropna(subset='ssha',inplace=True)
+df.dropna(subset='ssha', inplace=True)
 df.sort_values(by='time', inplace=True)
 
-df_kiel= df[df['latitude'] == latTGs[0][0]]
-df_kiel['ssha_demean'] = df_kiel['ssha']-df_kiel['ssha'].mean()
+# for filename in tqdm(fileSWOT, desc='SWOT files'):
+#     file_path = os.path.join(SWOT_path, filename)
+#     ds = xr.open_dataset(file_path)
 
-df_alte = df[df['latitude'] == latTGs[1][0]]
-df_alte['ssha_demean'] = df_alte['ssha']-df_alte['ssha'].mean()
+#     ds = ds.drop_dims('num_nadir')
+#     ds = ds[['time', 'ssha', 'latitude', 'longitude', 'dac']]
+    
+#     ssh_dac = ds.ssha+ds.dac
 
-df_tg_k = kiel_tg.reset_index()
-df_tg_k = df_tg_k[['TIME', 'SLEV']]
-df_tg_k['TIME'] = pd.to_datetime(df_tg_k['TIME'])
-df_tg_k.set_index('TIME', inplace=True)
-df_tg_k = df_tg_k[df_tg_k.index > pd.to_datetime('2020-01-01')]
+#     # Extract data from variables
+#     lon = ds['longitude'].values.flatten()
+#     lat = ds['latitude'].values.flatten()
+#     ssh = ds['ssha'].values.flatten()
+#     ssh_dac = ssh_dac.values.flatten()
+#     # ssh = ds['ssha'].values.flatten()
+
+#     time_values = ds['time'].values  # Adding a new
+#     time = np.tile(time_values[:, np.newaxis], (1, 69)).flatten()  # Not efficient
+
+#     # Find indices of non-NaN values
+#     valid_indices = np.where(~np.isnan(ssh))
+#     lonSWOT = lon[valid_indices]
+#     latSWOT = lat[valid_indices]
+#     timeSWOT = time[valid_indices]
+#     ssh = ssh[valid_indices]
+#     ssh_dac = ssh_dac[valid_indices]
+
+#     # Loop through each tide gauge location
+#     for idx, (lon_tg, lat_tg) in enumerate(zip(lonTGs, latTGs)):
+
+#         # d = np.sqrt((lonSWOT - lon_tg) ** 2 + (latSWOT - lat_tg) ** 2)
+
+#         # if np.min(d) > thres:
+#         #     continue
+
+#         # # Mask distances greater than threshold
+#         # mask = d <= thres
+#         # alon = np.where(mask, lonSWOT, np.nan)
+#         # alat = np.where(mask, latSWOT, np.nan)
+
+#         # Calculate distance for each data point
+#         distances = haversine(lonSWOT, latSWOT, lon_tg, lat_tg)
+
+#         in_radius = distances <= dmin
+
+#         # Average nearby SSH values (if any)
+#         if np.any(in_radius):
+#             print(f'there is data')
+
+#             ssh_tmp = np.nanmean(ssh[in_radius])
+#             ssh_serie = ssh_tmp * 100  # Convert to centimeters (cm)
+#             time_serie = timeSWOT[in_radius][~np.isnan(timeSWOT[in_radius])][0]  # Picking the first value of time within the radius
+#             ssh_dac_serie = np.nanmean(ssh_dac[in_radius])*100  # To cm
+            
+#             # Store the latitudes and longitudes of SWOT within the radius
+#             lat_within_radius = latSWOT[in_radius]
+#             lon_within_radius = lonSWOT[in_radius]
+
+#             # Store closest  distance and number of points used for the average
+#             min_distance_point = distances[in_radius].min()
+#             n_idx = sum(in_radius)  # How many values are used for compute the mean value
+
+#         else:
+#             # print(f'there is no data')
+#             ssh_serie = np.nan  # No data within radius (remains NaN)
+#             time_serie = timeSWOT[in_radius][~np.isnan(timeSWOT[in_radius])]
+#             n_idx = np.nan  # Number of points for the average within the radius
+#             min_distance_point = np.nan
+#             ssh_dac_serie = np.nan
+
+#             # If there's no SWOT data within the radius, set latitudes and longitudes to None
+#             lat_within_radius = None
+#             lon_within_radius = None
+#             # print(f"No SWOT data within {dmedia} km radius of tide gauge {sorted_names[idx]}")
+
+#         # Create a dictionary to store tide gauge and SWOT data
+#         selected_data = {
+#             # "station": filename,  # Access station name
+#             "longitude": lon_tg[0],  # Longitude of tide gauge
+#             "latitude": lat_tg[0],  # Latitude of tide gauge
+#             "ssha": ssh_serie, # Retrieved SSH value
+#             "ssha_dac": ssh_dac_serie,
+#             # "ssha_raw": ssh[in_radius],  # Raw SSH values within the radius
+#             "time": time_serie,
+#             "n_val": n_idx,  # Number of points for the average within the radius
+#             "lat_within_radius": lat_within_radius,  # Latitudes of SWOT within the radius
+#             "lon_within_radius": lon_within_radius,   # Longitudes of SWOT within the radius
+#             "min_distance": min_distance_point,  # Closest distance within the radius
+#             }
+#         all_altimetry_timeseries.append(selected_data)
+
+# df = pd.DataFrame(all_altimetry_timeseries)
+# df.dropna(subset='ssha',inplace=True)
+# df.sort_values(by='time', inplace=True)
+
+# df_kiel= df[df['latitude'] == latTGs[0][0]]
+# df_kiel['ssha_demean'] = df_kiel['ssha']-df_kiel['ssha'].mean()
+
+# df_alte = df[df['latitude'] == latTGs[1][0]]
+# df_alte['ssha_demean'] = df_alte['ssha']-df_alte['ssha'].mean()
+
+# df_tg_k = kiel_tg.reset_index()
+# df_tg_k = df_tg_k[['TIME', 'SLEV']]
+# df_tg_k['TIME'] = pd.to_datetime(df_tg_k['TIME'])
+# df_tg_k.set_index('TIME', inplace=True)
+# df_tg_k = df_tg_k[df_tg_k.index > pd.to_datetime('2020-01-01')]
 
 
-df_tg_a = Alte_tg.reset_index()
-df_tg_a = df_tg_a[['TIME', 'SLEV']]
-df_tg_a['TIME'] = pd.to_datetime(df_tg_a['TIME'])
-df_tg_a.set_index('TIME', inplace=True)
-df_tg_a = df_tg_a[df_tg_a.index > pd.to_datetime('2020-01-01')]
+# df_tg_a = Alte_tg.reset_index()
+# df_tg_a = df_tg_a[['TIME', 'SLEV']]
+# df_tg_a['TIME'] = pd.to_datetime(df_tg_a['TIME'])
+# df_tg_a.set_index('TIME', inplace=True)
+# df_tg_a = df_tg_a[df_tg_a.index > pd.to_datetime('2020-01-01')]
 
-df_tg_k['SLEV_demean'] = df_tg_k['SLEV'] - df_tg_k['SLEV'].mean()
-df_tg_a['SLEV_demean'] = df_tg_a['SLEV'] - df_tg_a['SLEV'].mean()
-
-
-# Detide
-
-df_tg_k = detide(df_tg_k.index, df_tg_k['SLEV_demean'], latitude=latTGs[0])
-df_tg_a = detide(df_tg_a.index, df_tg_a['SLEV_demean'], latitude=latTGs[1])
+# df_tg_k['SLEV_demean'] = df_tg_k['SLEV'] - df_tg_k['SLEV'].mean()
+# df_tg_a['SLEV_demean'] = df_tg_a['SLEV'] - df_tg_a['SLEV'].mean()
 
 
-plt.plot(df_tg_k.index[-5000:], df_tg_k['ssha_detided'][-5000:]*100, zorder=0)
-plt.scatter(df_kiel['time'], df_kiel['ssha_demean'], c='r', zorder = 1)
-plt.xticks(rotation=45)
+# # Detide
+
+# df_tg_k = detide(df_tg_k.index, df_tg_k['SLEV_demean'], latitude=latTGs[0])
+# df_tg_a = detide(df_tg_a.index, df_tg_a['SLEV_demean'], latitude=latTGs[1])
+
+# Read model data SCHISM
+
+# model23 = xr.open_dataset(f'{model_path}merged_elevation_2023.nc')
+# model24 = xr.open_dataset(f'{model_path}merged_elevation_2024.nc')
+
+# model = xr.concat([model23, model24], dim='time')
+
+
+# plt.plot(df_tg_k.index[-5000:], df_tg_k['ssha_detided'][-5000:]*100, zorder=0)
+# plt.scatter(df_kiel['time'], df_kiel['ssha_demean'], c='r', zorder = 1)
+# plt.xticks(rotation=45)
+
+
+# lonlatbox = [9, 12, 53, 59]
+# # Extract the specific time slice
+# time_index = 0  # specify the time index you want to plot
+# sea_elevation = model24['elevation'].isel(time=time_index).values
+
+# # Extract the coordinates
+# lon = model24['SCHISM_hgrid_node_x'].values
+# lat = model24['SCHISM_hgrid_node_y'].values
+
+# # Create a plot using Cartopy
+# fig, ax = plt.subplots(figsize=(10.5, 11), subplot_kw=dict(projection=ccrs.PlateCarree()))
+
+# ax.set_extent(lonlatbox)
+
+# # Add features to the map
+# ax.add_feature(cfeature.COASTLINE)
+# ax.add_feature(cfeature.BORDERS, linestyle=':')
+
+# # Plot the sea elevation data
+# contour = ax.contourf(lon, lat, sea_elevation, transform=ccrs.PlateCarree(), cmap='viridis')
+
+# # Add a colorbar
+# cbar = plt.colorbar(contour, ax=ax, orientation='vertical', pad=0.02)
+# cbar.set_label('Sea Elevation (m)')
+
+# # Set plot title
+# plt.title(f'Sea Elevation at Time Index {time_index}')
 
 
 
@@ -433,9 +541,6 @@ plt.xticks(rotation=45)
     # ax.legend()
     # plt.savefig(f'{names_tg[idx]}_detided.png')
     # plt.close()
-
-
-
 
 
 # lolabox = [-94, -81, 11, 27]
